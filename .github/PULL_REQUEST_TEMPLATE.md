b1b7ad55ebed399585a46f296fe1e3a5579e384b
杨泽予 <yangzeyu@yangzeyudeMacBook-Pro.local>
2022-08-07 02:27:28 -0400

    adding files

---
+++ .github/PULL_REQUEST_TEMPLATE.md
@@ -0,0 +1,19 @@
+List of fixed issues (if they exist):
+
+-
+-
+-
+
+Demos work on:
+
+- [ ] iOS Safari
+- [ ] iOS Firefox
+- [ ] iOS Chrome
+- [ ] Android Chrome
+- [ ] Android Firefox
+- [ ] macOS Safari
+- [ ] macOS Firefox
+- [ ] macOS Chrome
+- [ ] Windows Chrome
+- [ ] Windows Firefox
+
---
+++ vite.config.js
@@ -0,0 +1,22 @@
+// vite.config.js
+const path = require('path')
+const { defineConfig } = require('vite')
+
+module.exports = defineConfig({
+    root: '.',
+		server: {
+			open: '/src/index.html',
+    	fs: {
+				// Allow serving files from one level up to the project root
+				allow: ['..']
+    	}
+  	},
+    build: {
+        outDir: './dist',
+        lib: {
+            entry: path.resolve(__dirname, 'src/niivue.js'),
+            name: 'niivue',
+            fileName: (format) => `niivue.${format}.js`
+        },
+    }
+})
